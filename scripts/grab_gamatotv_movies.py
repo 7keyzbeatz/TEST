735f853c2551f2c2a2385bb1ce406225ec2f150e
7Keyz Beatz <114307671+7keyzbeatz@users.noreply.github.com>
2024-08-15 19:08:57 +0300

    Update grab_gamatotv_movies.py

--- scripts/grab_gamatotv_movies.py
+++ scripts/grab_gamatotv_movies.py
@@ -2,10 +2,8 @@ import requests
 from bs4 import BeautifulSoup
 import json
 
-# Base URL of the page to scrape
+# Base URLs of the pages to scrape
 base_url = 'https://gamatotv.info/category/tainies'
-
-# List of URLs to scrape
 urls = [base_url, base_url + '/2']
 
 # List to store all movie data
@@ -24,14 +22,14 @@ for url in urls:
         title_tag = post.find('h1', class_='post-title')
         
         # Check if the title tag is found
-        if title_tag is not None:
+        if title_tag:
             title = title_tag.get_text(strip=True)
         else:
             # Skip this post if no title is found
             continue
         
         # Extract post ID from the div ID
-        post_id = post.get('id').replace('post-', '')
+        post_id = post.get('id').replace('post-', '') if post.get('id') else 'unknown'
         
         # Append movie data to the list
         all_movies.append({
