55d651d18eb0b714b71cc8636d741805c4c9c446
7Keyz Beatz <114307671+7keyzbeatz@users.noreply.github.com>
2024-09-01 13:51:12 +0300

    Update clone_to_voe.py

--- scripts/clone_to_voe.py
+++ scripts/clone_to_voe.py
@@ -6,7 +6,7 @@ API_KEY = "oulcmpPHppZNamWvRNx8ZsVQixsyaAZXW3qbYmnE0xYVdFhznTIw79nqUm4gXxrH"
 
 # Function to clone a file to another Voe account
 def clone_file(file_code):
-    url = f"https://voe.sx/api/file/clone?key={API_KEY}&file_code={file_code}&fld_id=65461"
+    url = f"https://voe.sx/api/file/clone?key={API_KEY}&file_code={file_code}&fld_id=0"
     response = requests.get(url)
     if response.status_code == 200:
         data = response.json()
@@ -21,22 +21,22 @@ def clone_file(file_code):
 
 def process_json_file(input_file, output_file):
     with open(input_file, 'r') as f:
-        movies = json.load(f)
+        data = json.load(f)
     
-    # Assume movies is a list of dictionaries containing 'file_code'
+    movies = data.get("Movies", [])
     for movie in movies:
-        file_code = movie.get("file_code")
-        if file_code:
-            voe_id_backup = clone_file(file_code)
+        voe_id = movie.get("Voe_ID")
+        if voe_id:
+            voe_id_backup = clone_file(voe_id)
             if voe_id_backup:
                 movie["Voe_ID_Backup"] = voe_id_backup
     
     with open(output_file, 'w') as f:
-        json.dump(movies, f, indent=4)
+        json.dump(data, f, indent=4)
     
     print(f"Processed {len(movies)} movies and saved to {output_file}")
 
 if __name__ == "__main__":
     input_file = "data/movies_clone.json"  # The input JSON file
-    output_file = "movies_with_backup.json"  # The output JSON file
+    output_file = "data/movies_with_backup.json"  # The output JSON file
     process_json_file(input_file, output_file)
